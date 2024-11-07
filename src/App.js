7fb222152addb5a62a82cc5137f1b255ba456e8b
AppleBee12 <mieldeluna@naver.com>
2024-11-07 15:19:14 +0900

    content변경

--- src/App.js
+++ src/App.js
@@ -2,42 +2,59 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import BoardList from './BoardList';
 import Write from './Write';
+import View from './View';
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import React, { Component } from 'react'
+function App(){
+  const [isModifyMode, setIsModifyMode] = useState(false);
+  const [isComplete, setIsComplete] = useState(true);
+  const [boardId, setBoardId] = useState(0);
+  const [redirecToWrite, setRedirecToWrite] = useState(false);
+  const [redirectToHome, setRedirectToHome] = useState(false);
 
-export default class App extends Component {
-  state = {
-    isModifyMode :false, //수정모드
-    isComplete:true, //렌더 완료(목록 출력 완료)
-    boardId:0 //수정,삭제할 글 번호
-  }
-
-  handleModify = (checkList)=>{
-    if(checkList.length === 0){
+  const handleModify = (checkList) => {
+    if (checkList.length === 0) {
       alert('수정할 게시글을 선택하세요');
-    } else if(checkList.length > 1){
+    } else if (checkList.length > 1) {
       alert('하나의 게시글만 선택하세요');
+    } else{
+      setIsModifyMode(true);
+      setBoardId(checkList[0]);
+      setRedirecToWrite(true);
     }
-    this.setState({
-      isModifyMode:checkList.length === 1
-    });
-    
-    this.setState({
-      boardId:checkList[0] || 0
-    });
-
   }
 
-  render() {
-    return (
+  const handleCancel = () => {
+    setIsModifyMode(false);
+    setIsComplete(false);
+    setBoardId(0);
+    setRedirectToHome(true);
+  }
+
+  useEffect(()=>{
+    if(redirectToHome) setRedirectToHome(false);
+    if(redirecToWrite) setRedirecToWrite(false);
+  },[redirecToWrite,redirectToHome])
+
+  return(
+    <BrowserRouter>
       <div className="container">
         <h1>React Board</h1>
-        <BoardList handleModify={this.handleModify}/>
-        <Write 
-          isModifyMode={this.state.isModifyMode}
-          boardId = {this.state.boardId}
-        />
+        {redirecToWrite && <Navigate to="/write"  />} 
+        {redirectToHome && <Navigate to="/"  />} 
+        <Routes>
+          <Route path="/" element={<BoardList isComplete={isComplete} handleModify={handleModify} />} />
+          <Route path="/write" element={<Write 
+            isModifyMode={isModifyMode}
+            boardId={boardId}
+            handleCancel={handleCancel}
+          />}
+          />
+          <Route path="/view/:id" element={<View/>}/>
+        </Routes>
       </div>
-    )
-  }
+    </BrowserRouter>
+  )
 }
+export default App;
--- src/Write.js
+++ src/Write.js
@@ -1,75 +1,116 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
-export default class Write extends Component {
-  state= {
-    isModifyMode:false,
-    title:'',
-    content:''
-  }
-  write = (e)=>{
+
+const Write = ({boardId,isModifyMode,handleCancel})=>{
+  const navigate = useNavigate();
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e)=>{
+    console.log(e.target.files[0]);
+    setImage(e.target.files[0]);
+  }  
+
+  const write = (e)=>{
     e.preventDefault();
-    Axios.post('http://localhost:8000/insert',{
-      title:this.state.title,
-      content:this.state.content
+    const formData = new FormData(); 
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("image", image);
+
+    Axios.post('http://34.47.112.240:8000/insert',formData,{
+      headers:{"Content-Type":"multipart/form-data"}
     })
     .then((res) => {
-      console.log(res);
+      navigate('/'); //등록 완료후 홈으로 이동
+    })
+    .catch((e)=> {
+      console.log(e);
+    });  
+  }
+  const update = (e)=>{
+    e.preventDefault();
+    Axios.post('http://34.47.112.240:8000/update',{
+      title:title,
+      content:content,
+      id:boardId //수정할 글 번호
+    })
+    .then((res) => {
+      setTitle('');
+      setContent('');
+      setIsEditMode(false);
+      handleCancel();
+      //글 수정 완료후 수정모드 -> false로 변경, 목록 다시 조회, boardID 초기화
     })
     .catch((e)=> {
       // 에러 핸들링
       console.log(e);
     });  
   }
-  update = (e)=>{
-    e.preventDefault();
-    Axios.post('http://localhost:8000/update',{
-      title:this.state.title,
-      content:this.state.content,
-      id:this.props.boardId //수정할 글 번호
-    })
-    .then((res) => {
-      this.setState({
-        title:'',
-        content:''
-      });
-      this.props.handleCancel();
-    })
-    .catch((e)=> {
-      // 에러 핸들링
-      console.log(e);
-    });  
-  }
-  datail = () =>{
+  const detail = () =>{
     //글번호에 맞는 데이터 조회, 글 결과를 title, content반영, 수정모드 true    
+    Axios.get(`http://34.47.112.240:8000/detail?id=${boardId}`)
+    .then((res) => {
+      const {data} = res;  
+      setTitle(data[0].BOARD_TITLE);
+      setContent(data[0].BOARD_CONTENT);
+      setIsEditMode(true);
+    })
+    .catch((e)=> {
+      // 에러 핸들링
+      console.log(e);
+    });     
   }
-  //this.prop.isModifyMode에 변동사항이 생기면 detail 함수 실행, componentDidUpdate 함수로 
-  
+  useEffect(()=>{
+    if(isModifyMode && boardId){
+      detail();
+    }
+  },[isModifyMode,boardId])
 
-  handleChange = (e)=>{
-    this.setState({
-      [e.target.name]:e.target.value //계산된 속성
-    });
-    console.log(this.state);
-  }
+  const handleChangeTitle = (e)=> setTitle(e.target.value);
+  const handleChangeContent = (e)=> setContent(e.target.value);
+
+
+
+  return (      
+    <Form>
+      <Form.Group className="mb-3" controlId="title">
+        <Form.Label>제목</Form.Label>
+        <Form.Control type="text" name="title" value={title} placeholder="제목을 입력하세요" onChange={handleChangeTitle}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="content">
+        <Form.Label>내용</Form.Label>
+        <Form.Control as="textarea" name="content" value={content} rows={3} onChange={handleChangeContent} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="title">
+        <Form.Label>이미지</Form.Label>
+        <Form.Control type="file" accept="image/*" onChange={handleImageChange}/>
+      </Form.Group>      
+      <div className="d-flex gap-1">
+        <Button variant="primary" type="submit" onClick={isEditMode ? update : write}>{isEditMode ? '수정완료' : '입력완료'}</Button>
+        <Link to="/" className="btn btn-secondary">취소</Link>         
+      </div>      
+    </Form>
+  )
+}
+
+export default Write;
+/*
+export default class Write extends Component {
+ 
+
+
   render() {
-    return (      
-      <Form>
-        <Form.Group className="mb-3" controlId="title">
-          <Form.Label>제목</Form.Label>
-          <Form.Control type="text" name="title" placeholder="제목을 입력하세요" onChange={this.handleChange}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="content">
-          <Form.Label>내용</Form.Label>
-          <Form.Control as="textarea" name="content" rows={3} onChange={this.handleChange} />
-        </Form.Group>
-        <div className="d-flex gap-1">
-          <Button variant="primary" type="submit" onClick={this.state.isModifyMode ? this.update : this.write}>{this.state.isModifyMode ? '수정완료' : '입력완료'}</Button>
-          <Button variant="secondary" type="reset">취소</Button>          
-        </div>      
-      </Form>
-    )
+    if(this.state.redirect){
+      return <Navigate to="/"/>; //글쓰기(수정)이 완료되면 홈으로 이동
+    }
+
   }
 }
+  */
